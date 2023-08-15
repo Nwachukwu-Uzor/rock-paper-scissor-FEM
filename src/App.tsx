3e75c622ee01bf6fcfaf1837297552eadb658c73
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-08-15 13:00:21 +0100

    add styling to toggle rules modal button

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,22 @@ const App: React.FC = () => {
       <section className="w-[90%] max-w-[800px]">
         <Header score={0} />
         <Options />
-        <button onClick={handleToggleRulesModal}>Open Rules</button>
-        <Modal open={openRulesModal} handleDismiss={handleToggleRulesModal}>
-          <img src="/assets/images/rpsls-game-rules.png" alt="" />
+        <div className="fixed bottom-0 right-0 flex justify-end py-4 pr-6">
+          <button
+            onClick={handleToggleRulesModal}
+            className="text-white px-4 py-2 rounded-md border-2 border-white hover:opacity-80 active:scale-105 duration-150"
+          >
+            Open Rules
+          </button>
+        </div>
+        <Modal
+          open={openRulesModal}
+          handleDismiss={handleToggleRulesModal}
+          dismissible
+        >
+          <div className="bg-white px-2 lg:px-3 py-4 max-w-[400px] rounded-md shadow-md">
+            <img src="/assets/images/rpsls-game-rules.png" alt="" />
+          </div>
         </Modal>
       </section>
     </main>
