b73e493241dff06bd1493648eb001096a1196979
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-08-15 12:30:18 +0100

    adds the modal to the root App component and pass in the game rule image as modal children

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { Header, Options } from "./components";
+import React, { useState } from "react";
+import { Header, Options, Modal } from "./components";
 
 const App: React.FC = () => {
+  const [openRulesModal, setOpenRulesModal] = useState(false);
+
+  const handleToggleRulesModal = () => {
+    setOpenRulesModal((opened) => !opened);
+  };
+
   return (
     <main className="flex items-center justify-center py-6 lg:py-8">
       <section className="w-[90%] max-w-[800px]">
         <Header score={0} />
         <Options />
+        <button onClick={handleToggleRulesModal}>Open Rules</button>
+        <Modal open={openRulesModal} handleDismiss={handleToggleRulesModal}>
+          <img src="/assets/images/rpsls-game-rules.png" alt="" />
+        </Modal>
       </section>
     </main>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./header";
-export { Options } from "./options"
+export { Options } from "./options";
+export { Modal } from "./modal";
