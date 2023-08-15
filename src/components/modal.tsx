e8dc282688a298e914caea20b78fbd3c747d2eae
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-08-15 13:29:40 +0100

    fixes modal close event firing when modal backdrop is clicked instead of backdrop

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, MouseEvent } from "react";
 
 interface Props extends ComponentProps<"div"> {
   open: boolean;
@@ -12,12 +12,16 @@ export const Modal: React.FC<Props> = ({
   dismissible,
   children,
 }) => {
+  const handleModalContentClick = (event: MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return open ? (
     <section
       onClick={dismissible ? handleDismiss : undefined}
       className="fixed top-0 left-0 bottom-0 right-0 z-40 flex items-center justify-center"
     >
-      <div className="relative z-50">{children}</div>
+      <div className="relative z-50" onClick={handleModalContentClick}>{children}</div>
       <div
         className={`absolute top-0 left-0 bottom-0 right-0 z-20 backdrop-blur-sm bg-black/50 ${
           dismissible ? "cursor-pointer" : ""
